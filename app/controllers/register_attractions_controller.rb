7f6d0afc41752a3d19ae7fbafad4020baf29957c
Augusto Vilela <avilelateixeira@gmail.com>
2018-06-27 03:13:28 +0000

    Cadastro de atrações funcionando, faltando somente acertos no front

--- app/controllers/register_attractions_controller.rb
+++ app/controllers/register_attractions_controller.rb
@@ -1,8 +1,20 @@
 class RegisterAttractionsController < ApplicationController
 	before_action :authenticate_company!
-	
+
 	def index
-		@attractions = Attraction.where(company_id: current_company.id)
+		@attractions = Attraction.where(company_id: current_company.id).order('id ASC')
+	end
+
+	def new
+		@attraction = Attraction.new
+	end
+
+	def create
+		@attraction = Attraction.new(attractive_params)
+		@attraction.company_id = current_company.id
+    	@attraction.save
+
+    	redirect_to root_path
 	end
 
 	def show
@@ -11,6 +23,9 @@ class RegisterAttractionsController < ApplicationController
 	def edit
 	end
 
-	def destroy
-	end
+	private 
+		def attractive_params
+			params.require(:attraction).permit(:name,:address,:duration_time,:max_people,:image,:price,:company_id)
+		end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'register_attractions/index'
   get 'register_attractions/show'
   get 'register_attractions/edit'
-  get 'register_attractions/destroy'
+  get 'register_attractions/new'
+  post 'register_attractions/create'
+
   devise_for :companies
   
   root "register_attractions#index"

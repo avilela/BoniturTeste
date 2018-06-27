c02d45029ebbffb7a0b9938e2573c99ad64d2ada
Augusto Vilela <avilelateixeira@gmail.com>
2018-06-27 02:17:49 +0000

    algumas melhoras de layout e acerto no back-end do controller RegisterAttractionsController

--- app/controllers/register_attractions_controller.rb
+++ app/controllers/register_attractions_controller.rb
@@ -1,6 +1,8 @@
 class RegisterAttractionsController < ApplicationController
 	before_action :authenticate_company!
+	
 	def index
+		@attractions = Attraction.where(company_id: current_company.id)
 	end
 
 	def show

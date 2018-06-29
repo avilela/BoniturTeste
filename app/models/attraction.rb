c188154325459c111b68bf0f975d3fb9cb98b7e8
Augusto Vilela <avilelateixeira@gmail.com>
2018-06-29 03:05:40 +0000

    formatações e validações

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,4 +3,14 @@ class Attraction < ApplicationRecord
   	belongs_to :company
 
   	validates_presence_of  :name, :address, :duration_time, :max_people, :price, :company_id
+  	validate :image?, :is_image?
+
+  	private 
+  		def image?
+      		errors.add(:base, 'Por favor insira uma imagem para atração.') unless image.attached?
+	    end
+
+	    def is_image?
+	    	errors.add(:base, 'Por favor insira um arquivo que seja uma imagem') unless image.blob.content_type.starts_with?('image/')
+	    end
 end

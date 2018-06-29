3d9ecfd3f8f562e28d974060e0be4264c725b88c
Augusto Vilela <avilelateixeira@gmail.com>
2018-06-29 03:35:07 +0000

    validações

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,7 @@ class Company < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
     has_many :attractives
-
+    validates :cnpj, uniqueness: true
     validates_presence_of :cnpj,:social_name, :fantasy_name, :password_confirmation
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 	validates_format_of :cnpj, :with => (/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/),  :multiline => true, :on => :create
@@ -13,4 +13,5 @@ class Company < ApplicationRecord
 	def to_s
 		self.fantasy_name
 	end
+
 end
